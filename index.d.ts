5f87479f9ac3b9e216b583610cd5bed2e29d89ca
bengreenier <ben@bengreenier.com>
2018-10-04 22:06:36 -0400

    v1.0.1 - patch index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,8 +1,6 @@
-declare module 'common-ext-tests' {
-  /**
-   * Includes common overlayed-app tests
-   */
-  function includeCommonTests(absPathToExtension: string) : void
+export = includeCommonTests
 
-  export = includeCommonTests
-}
+/**
+ * Includes common overlayed-app tests
+ */
+declare function includeCommonTests(absPathToExtension: string) : void
